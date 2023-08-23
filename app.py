0e227a29dc33d200df7719c66f545e7535759601
3braheem <ibraahimsiri@gmail.com>
2023-08-22 21:24:29 -0700

    bokeh graphs

--- app.py
+++ app.py
@@ -1,19 +1,20 @@
-import pandas as pd
-import matplotlib.pyplot as plt
-import numpy as np
-import mpld3
 from flask import Flask
+from flask import render_template
+from flask_htmx import HTMX
+from bokeh.plotting import figure, curdoc
+from bokeh.embed import components
+import numpy as np
 
 app = Flask(__name__)
+htmx = HTMX(app)
 
 
-@app.route("/plot")
+@app.route("/")
 def plot_to_web():
-    x = np.linspace(0, 2*np.pi, 200)
-    y = np.sin(x)
+    curdoc().theme = "night_sky"
+    p = figure(title="dark_minimal", width=400, height=400)
+    p.circle([1,2,3,6,8], [7,5,2,8,1], size=10)
 
-    fig, ax = plt.subplots()
-    ax.plot(x, y)
+    script, div = components(p)
 
-    html_str = mpld3.fig_to_html(fig)
-    return html_str
+    return render_template("index.html", script=script, div=div)
